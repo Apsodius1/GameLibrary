caaba9567483c279e84b83857df901fe5100db3f
Apsodius <augustin.cibotari@s.unibuc.ro>
2021-11-21 21:09:58 +0200

    functie cumparare jocuri vector de jocuri patch->string numele claselor la singular

--- main.cpp
+++ main.cpp
@@ -135,14 +135,26 @@ public:
 
 int main() {
 
-    joc Minecraft("Minecraft", "Sandbox", "1.17", 1.2, 2011, 2011, 115, 1);
+    joc Dota2("Dota 2", "MOBA", "7.30", 30, 2013, 110000, 0, 2);
+    joc Minecraft("Minecraft", "Sandbox", "1.17", 1.2, 2011, 20110, 115, 1);
+    joc RL("Rocket League", "Football", "2.07", 23.5, 2015, 100000, 100, 3);
+    joc TFM("TeamFight Manager", "Strategy", "1.4.5", 0.3, 2020, 25000, 35, 4);
+    joc ETG("Enter the Gungeon", "Bullet hell", "2.1.92", 2, 2016, 340000, 33, 5);
     joc joc3(Minecraft);
     joc joc2;
     joc2 = joc3;
     std::cout << joc3;
     Minecraft.sale(30);
     Minecraft.actualizare(0.12, "1.173");
+    RL.sale(100);
+    std::cout<<RL;
 
+    utilizator bot1("BOT1", " I'm the first bot", 10000,0);
+    utilizator bot2("BOT2", " I'm the second bot", 10000,0);
+    utilizator bot3("BOT3", " I'm the third bot", 10000,0);
+    utilizator bot4("BOT4", " I'm the fourth bot", 10000,0);
+    utilizator bot5("BOT5", " I'm the fifth bot", 10000,0);
+    utilizator bot6("BOT6", " I'm bot number 6", 10000,0);
     utilizator P1;
     std::cout << P1;
     utilizator P2("Augustin", "-", 100, 0);
